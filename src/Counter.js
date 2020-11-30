70730643f18e58f0890bfaa66fe2ecd216a24a44
Kim Taenam <cr000@naver.com>
2020-11-30 14:21:30 +0900

    Counter UI

---
+++ src/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <p>Counter Component State: 0</p>
+            <button type="button">+</button>
+            <button type="button">-</button>
+        </div>
+    );
+};
